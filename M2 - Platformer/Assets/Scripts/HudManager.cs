9908f06d0181de52b0a02f32a930d03d1befaeaa
guyserious <bernardorourke1@gmail.com>
2022-05-02 16:26:40 +0100

    Bug 5 Found and Fixed
    Reference added to score variable to display on HUD

--- M2 - Platformer/Assets/Scripts/HudManager.cs
+++ M2 - Platformer/Assets/Scripts/HudManager.cs
@@ -17,7 +17,10 @@ public class HudManager : MonoBehaviour {
     // Show up to date stats of the player
     public void ResetHud()
     {
-        scoreLabel.text = "Score: ";
+        scoreLabel.text = "Score: " + GameManager.instance.score.ToString();
+        // Bug 5 Found, reference was missing here to score variable located within GameManager 
+        // Added and pulled in as string to display on HUD 
+        
     }
 	
 	
